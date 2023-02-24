eb1c6da2a52114cf25bc9aca36c6a60cbee10479
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-02-24 14:09:20 -0500

    Created a 16 x 16 grid and mouseover event

---
+++ script.js
@@ -0,0 +1,16 @@
+// create 16 x 16  grid of divs
+
+const container = document.getElementById("container");
+
+for (let i =0; i < 16 * 16; i++){
+const newDiv = document.createElement("div");
+newDiv.className = "row";
+container.appendChild(newDiv);
+}
+
+const overSquare = document.querySelectorAll(".row");
+for (let i =0; i < overSquare.length; i++){
+overSquare[i].addEventListener("mouseover", e =>{
+  overSquare[i].style.backgroundColor ="green";
+})
+}
