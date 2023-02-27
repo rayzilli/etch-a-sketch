dd2bc54a3c6827bbcd440e9e561523912662e00b
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-02-27 13:08:35 -0500

    Grid changes with slider
    value starts with slider default and changes when moved
    event listeners for both grid size and mouseover color changes

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-// create 16 x 16  grid of divs
+// create grid
+const slider = document.getElementById("range");
+createBoard(slider.value);
 
-const container = document.getElementById("container");
+slider.addEventListener("input", (e) =>{
+    console.log(slider.value);
+    document.getElementById("amount").innerHTML = slider.value;
+    createBoard(slider.value);
+})
 
+function createBoard(amount){
+    const container = document.getElementById("container");
+    container.innerHTML ="";
+    container.style.gridTemplateColumns = `repeat(${amount}, 1fr)`;
+    const squareAmount = amount;
+    for (let i =0; i < amount * amount; i++){
+    const newDiv = document.createElement("div");
+    newDiv.className = "square";
+    container.appendChild(newDiv);
+    reDraw();
+    };
+    
+    }
 
-for (let i =0; i < 16 * 16; i++){
-const newDiv = document.createElement("div");
-newDiv.className = "row";
-container.appendChild(newDiv);
-}
-
-const overSquare = document.querySelectorAll(".row");
+function reDraw(){
+const overSquare = document.querySelectorAll(".square");
 for (let i =0; i < overSquare.length; i++){
 overSquare[i].addEventListener("mouseover", e =>{
   overSquare[i].style.backgroundColor ="purple";
 })
 }
+}
 
-const slider = document.getElementById("range");
-slider.addEventListener("change", (e) =>{
-    console.log(slider.value);
-    document.getElementById("amount").innerHTML = slider.value;
-})
+
+
+
+
+
+
+
+
 
 
