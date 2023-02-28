c99dccb6013109fda8677c60848664170476e9eb
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-02-28 11:51:09 -0500

    added random color generator and rainbow button

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ clear.addEventListener("click", () => createBoard(slider.value, myColor.value))
 const myColor = document.getElementById("myColor");
 myColor.addEventListener("input", () => reDraw(myColor.value));
 
+const rainbow =document.getElementById("rainbow");
+rainbow.addEventListener("click", ()=> reDraw("rainbow"));
+
+
 slider.addEventListener("input", (e) =>{
     document.getElementById("amount").innerHTML = slider.value;
     createBoard(slider.value, myColor.value);
@@ -33,15 +37,33 @@ function createBoard(amount, color){
 
 function reDraw(newColor){
 const overSquare = document.querySelectorAll(".square");
+if (newColor == "rainbow"){
+    for(let i=0; i<overSquare.length; i++){
+        overSquare[i].addEventListener("mouseover", e =>{
+            overSquare[i].style.backgroundColor = colorGenerator(); 
+        })
+    }
+}
+else{
 for (let i =0; i < overSquare.length; i++){
 overSquare[i].addEventListener("mouseover", e =>{
   overSquare[i].style.backgroundColor = newColor;
 })
 }
 }
+}
 
 
+function colorGenerator(){
+    let red, green , blue;
+    red = Math.floor(Math.random() * 256);
+    blue = Math.floor(Math.random() * 256);
+    green = Math.floor(Math.random() * 256);
+    let color = `rgb(${red},${blue},${green})`;
+    return color; 
+}
 
+colorGenerator();
 createBoard(slider.value, myColor.value);
 
 
